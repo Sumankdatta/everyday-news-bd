225c0d7e690ecc41202c7a9b2061947f00310896
Sumankdatta <sumankdatta18@gmail.com>
2022-09-04 17:17:07 +0600

    new blog added

--- js/news.js
+++ js/news.js
@@ -66,7 +66,7 @@ const newsTitle=newsCards=>{
      
    const creatNewDiv=document.createElement('div');
    creatNewDiv.innerHTML=`
-  <div onclick="displayNewsLoad('${cards._id}')" class="row g-0 mb-5" style="border-top:1px solid gray;border-bottom:1px solid gray;">
+  <div class="row g-0 mb-5" style="border-top:1px solid gray;border-bottom:1px solid gray;">
           <div class="col-md-4 px-4">
             <img src="${cards.image_url}" class="img-fluid rounded-start me-5" style="width:1000px;height:100%;" alt="...">
           </div>
@@ -88,7 +88,11 @@ const newsTitle=newsCards=>{
                         <p>View: ${cards.total_view}</p>
                      </div>  
  
-                    
+                    <div>
+                    <button onclick="displayNewsLoad('${cards._id}')"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    View Details
+                  </button>
+                    </div>
               </div>
             </div>
           </div>
@@ -126,30 +130,10 @@ const displayNewsDetail=cards=>{
   
   creatDivContainer.innerHTML=`
   
-<div class="card mb-3">
-<img src="${cards.image_url ? cards.image_url: 'no'}" class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">${cards.title}</h5>
-  <p class="card-text">${cards.details}</p>
-  <div class="d-flex d-flex justify-content-around mt-4">
-          <div class="d-flex">
-            
-          <img src="${cards.author.img}" class="rounded-circle" style="height:50px;width:50px;" alt="...">
-          <div class="ms-3">
-              <h6>${cards.author.name ? cards.author.name:'No name'}</h6>
-              <p>${cards.author.published_date}</p>
-          </div>
-      </div>
-              
-       <div>
-          <p>View: ${cards.total_view ? cards.total_view:'No view'}</p>
-       </div>   
   
-</div>
-
-
-</div>
-</div>`
+  
+  
+  `
 
         newCardsContainer.appendChild(creatDivContainer) 
        
